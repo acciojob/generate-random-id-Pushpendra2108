07156ed8d29ce32c8b6e234e654780c94727345a
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 17:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function makeid(l) {
   // write your code here
-}
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+  
+    for (let i = 0; i < l; i++) {
+      const randomIndex = Math.floor(Math.random( ) * characters.length); 
+                        // math.floor( 0.50         *     62          )
+                        //math.floor ( 31.0)
+                        // 31 
+      result = result +  characters[randomIndex];
+               // " " +   characters[31]
+               //" "  +    f =  " f "
+    }
+  
+    return result;
+  }
+	
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
